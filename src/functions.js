2604d8b371322dcf7be175b7f5b6240f6c721336
BomberFish <bomberfish@bomberfish.ca>
2024-04-30 19:03:17 -0400

    add anura file saving

--- src/functions.js
+++ src/functions.js
@@ -1092,6 +1092,7 @@ function file_new() {
 }
 
 async function file_open() {
+	
 	const { file, fileHandle } = await systemHooks.showOpenFileDialog({ formats: image_formats })
 	open_from_file(file, fileHandle);
 }
